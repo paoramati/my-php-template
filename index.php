0c17a5e599066f18578444de0bb1a60b825169d7
paoramati <brandonmitchellpaul@gmail.com>
2019-06-29 20:51:28 +1200

    config

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 require('vendor/autoload.php');
 
 // define config constants
-define('RES_DIR', 'res/');
-define('JS_DIR', RES_DIR . 'js/');
-define('CSS_DIR', RES_DIR . 'css/');
+// define('RES_DIR', 'res/');
+// define('JS_DIR', RES_DIR . 'js/');
+// define('CSS_DIR', RES_DIR . 'css/');
 
 // add config from app.ini?
+// $config = parse_ini_file('app.ini');
